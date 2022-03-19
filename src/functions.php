65e9779a7048d032ca44ac501d32c6d2e7522079
knopsmedia <ignace@knopsmedia.be>
2022-03-19 13:52:26 +0100

    Updated collect() to use the new CollectionInterface BC

--- src/functions.php
+++ src/functions.php
@@ -3,15 +3,15 @@
 namespace Knops\Toolbox;
 
 use Carbon\{CarbonImmutable, CarbonInterface};
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use Knops\Toolbox\Doctrine\Collection\ArrayCollection;
+use Knops\Toolbox\Doctrine\Collection\CollectionInterface;
 
 function datetime(?string $time = null, ?string $timezone = null): CarbonInterface
 {
     return new CarbonImmutable($time, $timezone);
 }
 
-function collect(array $elements = []): Collection
+function collect(array $elements = []): CollectionInterface
 {
     return new ArrayCollection($elements);
 }
